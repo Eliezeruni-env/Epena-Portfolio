e988b35a7072c169e587c25fc8a2a7961b847db2
Eliezer pena <eliezer.pena@precisa.com.do>
2026-07-27 13:57:36 -0400

    eso e  eso

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Footer.scss'
+import '../assets/styles/Footer.scss';
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/Eliezeruni-env" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="www.linkedin.com/in/eliezer-alberto-peña-zabala-ab4aa82b1" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>A portfolio designed & built by <a href="https://github.com/Eliezeruni-env/Epena-Portfolio" target="_blank" rel="noreferrer">Eliezer Peña</a> with 💜</p>
     </footer>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,27 +1,43 @@
-import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Main.scss';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+// 1. Importa la imagen con una ruta válida usando barras diagonales
+import portfolioImg from "../assets/images/portfolioImg.jpg";
+import "../assets/styles/Main.scss";
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          {/* 2. Usa la variable de la imagen importada entre llaves */}
+          <img src={portfolioImg} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/Eliezeruni-env"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Eliezer Peña</h1>
           <p>Full Stack Engineer</p>
-
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a
+              href="https://github.com/Eliezeruni-env"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <LinkedInIcon />
+            </a>
           </div>
         </div>
       </div>
